6a0354f41d14abbb086736bea83ef1548c8554b5
Natty Davis <natty.davis79@gmail.com>
2017-09-28 21:59:46 -0400

    created the models folder with user.js for our functions when users signin,signup and verify

---
+++ models/users.js
@@ -0,0 +1,12 @@
+module.exports = {
+  signUp: async(req,res,next){
+    console.log('this is the signup function')
+  },
+  signIn: async(req,res,next){
+    console.log('this is the signIn function')
+  },
+  secret: async(req,res,next){
+    console.log('this is the secret function')
+  }
+
+}
